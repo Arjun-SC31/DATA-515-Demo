15d67066456ad0a0ba47ce332818bec2a605141c
Arjun Sharma <65405284+Arjun-SC31@users.noreply.github.com>
2023-03-06 19:36:46 -0800

    Update dummy_app1.py

--- myapp/dummy_app1.py
+++ myapp/dummy_app1.py
@@ -21,7 +21,7 @@ st.set_page_config(
 )
 
 # Reading the data
-df = pd.read_csv("../sampledata515.csv")
+df = pd.read_csv("https://drive.google.com/file/d/1Sp7MjufIbK5VHfP-6GTeEhSmle0OZ0UC/view?usp=share_link")
 df['Zip'] = df['Zip'].apply(str) # pgeocodes accepts string inputs for zip codes
     
 def zip_to_coordinates(zip_code):

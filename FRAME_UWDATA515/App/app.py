f9ac03621467123364e1e40f7725b1bacac8e9b8
Arjun Sharma <65405284+Arjun-SC31@users.noreply.github.com>
2023-03-12 03:25:35 -0700

    Update app.py

--- FRAME_UWDATA515/App/app.py
+++ FRAME_UWDATA515/App/app.py
@@ -19,7 +19,7 @@ st.set_page_config(
 )
 
 # Reading the data
-df = pd.read_csv("Food_and_Restaurant_Data.csv")
+df = pd.read_csv("../Food_and_Restaurant_Data.csv")
 df['zip_code'] = df['zip_code'].apply(str) # pgeocodes accepts string inputs for zip codes
 
 seattle_zips = ['98103', '98122', '98105', '98133', '98107', '98117', '98115',

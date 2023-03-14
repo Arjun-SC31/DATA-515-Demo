8b46c1676d50a5db11283506ed43b3a8906ff8d8
Arjun Sharma <65405284+Arjun-SC31@users.noreply.github.com>
2023-03-13 17:59:23 -0700

    Update app.py

--- App/app.py
+++ App/app.py
@@ -260,8 +260,8 @@ def main():
         else:
             pass
         placeholder.empty()
-        #st.balloons()
-        st.snow()
+        st.balloons()
+        #st.snow()
         st.header('Below are your food recommendations:')
         #st.write(final_filter.shape)
         restaurants = []
